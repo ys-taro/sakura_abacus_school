688257f49efcf1cf4a43daab074cda01515f968c
EC2 Default User <ec2-user@ip-172-31-35-221.ap-northeast-1.compute.internal>
2025-09-17 10:35:07 +0000

    [fix]rank_idカラムを追加するマイグレーションをすべてコメントアウト

--- db/migrate/20250911044329_add_rank_id_and_next_kente_rank_id_to_students.rb
+++ db/migrate/20250911044329_add_rank_id_and_next_kente_rank_id_to_students.rb
@@ -4,11 +4,11 @@ class AddRankIdAndNextKenteRankIdToStudents < ActiveRecord::Migration[6.1]
     #add_column :students, :next_kente_rank_id, :bigint
 
     # 外部キー制約を追加 (KenteRankモデルがある場合)
-    add_foreign_key :students, :kente_ranks, column: :rank_id
-    add_foreign_key :students, :kente_ranks, column: :next_kente_rank_id
+    #add_foreign_key :students, :kente_ranks, column: :rank_id
+    #add_foreign_key :students, :kente_ranks, column: :next_kente_rank_id
 
     # インデックス追加（検索性能向上）
-    add_index :students, :rank_id
-    add_index :students, :next_kente_rank_id
+    #add_index :students, :rank_id
+    #add_index :students, :next_kente_rank_id
   end
 end
